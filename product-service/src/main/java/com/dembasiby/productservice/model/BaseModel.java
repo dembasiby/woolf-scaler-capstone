fc40f1a7f37ca06170b4b8d6aa4f4b6a45a2409a
Demba Siby <dembasiby@gmail.com>
2025-12-15 08:10:27 +0000

    Merge remote-tracking branch 'origin/main'

---
+++ product-service/src/main/java/com/dembasiby/productservice/model/BaseModel.java
@@ -0,0 +1,26 @@
+package com.dembasiby.productservice.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter @Setter
+public class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    private boolean isDeleted;
+}
