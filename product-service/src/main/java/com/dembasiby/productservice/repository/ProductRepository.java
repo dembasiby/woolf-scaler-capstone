31cf1b1dff652d30c89e6ac39cffda9decb3553d
Demba Siby <dembasiby@gmail.com>
2025-12-16 16:04:11 +0000

    feat: add product repository

---
+++ product-service/src/main/java/com/dembasiby/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.dembasiby.productservice.repository;
+
+import com.dembasiby.productservice.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
