57cbb22c46fd3fddcb59959f589f1a9a6b6c49e4
Demba Siby <dembasiby@gmail.com>
2025-12-16 16:04:41 +0000

    feat: add product mapper

---
+++ product-service/src/main/java/com/dembasiby/productservice/mapper/ProductMapper.java
@@ -0,0 +1,23 @@
+package com.dembasiby.productservice.mapper;
+
+import com.dembasiby.productservice.dto.ProductDto;
+import com.dembasiby.productservice.model.Product;
+
+public class ProductMapper {
+
+   public static Product toEntity(ProductDto productDto) {
+       return null;
+   }
+
+   public static ProductDto toProductDto(Product product) {
+       return new ProductDto(
+               String.valueOf(product.getId()),
+               product.getTitle(),
+               product.getDescription(),
+               String.valueOf(product.getPrice()),
+               product.getImageUrl(),
+               product.getCategory().getId(),
+               product.getCategory().getName()
+       );
+   }
+}
