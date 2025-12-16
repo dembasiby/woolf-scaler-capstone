364fb7c9fded72ef7f2692b316bfd8abcfa722e8
Demba Siby <dembasiby@gmail.com>
2025-12-16 15:56:21 +0000

    feat: add category mapper

---
+++ product-service/src/main/java/com/dembasiby/productservice/mapper/CategoryMapper.java
@@ -0,0 +1,20 @@
+package com.dembasiby.productservice.mapper;
+
+import com.dembasiby.productservice.dto.CategoryDto;
+import com.dembasiby.productservice.model.Category;
+
+public class CategoryMapper {
+    public static CategoryDto toCategoryDto(Category category) {
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setId(category.getId());
+        categoryDto.setName(category.getName());
+        return categoryDto;
+    }
+
+    public static Category toCategory(CategoryDto categoryDto) {
+        Category category = new Category();
+        category.setId(categoryDto.getId());
+        category.setName(categoryDto.getName());
+        return category;
+    }
+}
