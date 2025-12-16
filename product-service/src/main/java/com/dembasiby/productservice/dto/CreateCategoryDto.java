8c52d6fd37005742f2ca7552e524cbc1c39e5704
Demba Siby <dembasiby@gmail.com>
2025-12-16 15:54:52 +0000

    feat: add category dtos

---
+++ product-service/src/main/java/com/dembasiby/productservice/dto/CreateCategoryDto.java
@@ -0,0 +1,17 @@
+package com.dembasiby.productservice.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class CreateCategoryDto {
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 100)
+    private String name;
+}
---
+++ product-service/src/main/java/com/dembasiby/productservice/dto/UpdateCategoryDto.java
@@ -0,0 +1,11 @@
+package com.dembasiby.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class UpdateCategoryDto {
+    private String name;
+}
