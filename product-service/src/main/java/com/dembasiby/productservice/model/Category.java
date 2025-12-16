7f20e6539e3c89ec60c63ebab1df2771cc77f8ad
Demba Siby <dembasiby@gmail.com>
2025-12-16 15:47:35 +0000

    Add name field to category entity

--- product-service/src/main/java/com/dembasiby/productservice/model/Category.java
+++ product-service/src/main/java/com/dembasiby/productservice/model/Category.java
@@ -1,6 +1,5 @@
 package com.dembasiby.productservice.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
@@ -15,6 +14,9 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class Category extends BaseModel {
+
+    private String name;
+
     @OneToMany(fetch = FetchType.EAGER)
     private List<Product> featuredProducts;
 }
