55ae88103bfe553300b898e174a46b9021303816
Demba Siby <dembasiby@gmail.com>
2026-01-02 16:44:44 +0000

    fix: update constraint to not null

--- product-service/src/main/java/com/dembasiby/productservice/dto/product/CreateProductDto.java
+++ product-service/src/main/java/com/dembasiby/productservice/dto/product/CreateProductDto.java
@@ -1,7 +1,6 @@
 package com.dembasiby.productservice.dto.product;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
@@ -21,6 +20,6 @@ public class CreateProductDto {
     @Positive
     private Double price;
 
-    @NotEmpty
+    @NotNull
     private Long categoryId;
 }
