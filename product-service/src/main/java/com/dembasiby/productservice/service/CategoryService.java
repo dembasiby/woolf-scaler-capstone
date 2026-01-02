aac0bf7b1e44f9d7e145597198b60cb3f62ee6f2
Demba Siby <dembasiby@gmail.com>
2026-01-02 16:41:53 +0000

    fix: remove unused methods

--- product-service/src/main/java/com/dembasiby/productservice/service/CategoryService.java
+++ product-service/src/main/java/com/dembasiby/productservice/service/CategoryService.java
@@ -3,7 +3,6 @@ package com.dembasiby.productservice.service;
 import com.dembasiby.productservice.dto.category.CategoryDto;
 import com.dembasiby.productservice.dto.category.CreateCategoryDto;
 import com.dembasiby.productservice.dto.category.UpdateCategoryDto;
-import com.dembasiby.productservice.dto.product.ProductCategoryDto;
 
 import java.util.List;
 
@@ -12,6 +11,5 @@ public interface CategoryService {
     CategoryDto updateCategory(Long id, UpdateCategoryDto categoryDto);
     CategoryDto getCategory(Long id);
     List<CategoryDto> getCategoryList();
-    List<ProductCategoryDto> getCategoryProducts(Long id);
     void deleteCategory(Long id);
 }
