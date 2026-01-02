1fc86f2ec38c6492c2c5db65ed9cd098c0d5fade
Demba Siby <dembasiby@gmail.com>
2026-01-02 16:40:33 +0000

    fix: remove unused methods

--- product-service/src/main/java/com/dembasiby/productservice/repository/CategoryRepository.java
+++ product-service/src/main/java/com/dembasiby/productservice/repository/CategoryRepository.java
@@ -1,8 +1,6 @@
 package com.dembasiby.productservice.repository;
 
-import com.dembasiby.productservice.dto.product.ProductCategoryDto;
 import com.dembasiby.productservice.model.Category;
-import com.dembasiby.productservice.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,5 +9,4 @@ import java.util.Optional;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findAllByIsDeletedFalse();
     Optional<Category> findByIdAndIsDeletedFalse(Long id);
-    List<ProductCategoryDto> getFeaturedProductsByCategoryId(Long id);
 }
