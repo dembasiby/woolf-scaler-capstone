0c3deba47c61b09ab35a6aab29830b93e010b3af
Demba Siby <dembasiby@gmail.com>
2026-01-02 16:52:55 +0000

    feat: add custom dto for product listing per category

--- product-service/src/main/java/com/dembasiby/productservice/dto/product/ProductCategoryDto.java
+++ product-service/src/main/java/com/dembasiby/productservice/dto/product/ProductCategoryDto.java
@@ -1,7 +1,10 @@
 package com.dembasiby.productservice.dto.product;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 @AllArgsConstructor
 public class ProductCategoryDto {
     private String id;
