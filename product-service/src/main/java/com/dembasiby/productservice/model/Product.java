787e511d69e0a806c04d7cbd5d8d49b8aa5e5109
Demba Siby <dembasiby@gmail.com>
2026-01-02 16:51:54 +0000

    feat: add productSpecifications field

--- product-service/src/main/java/com/dembasiby/productservice/model/Product.java
+++ product-service/src/main/java/com/dembasiby/productservice/model/Product.java
@@ -1,21 +1,25 @@
 package com.dembasiby.productservice.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
+@Table(name = "products")
 public class Product extends BaseModel {
     private String title;
     private String description;
     private Double price;
     private String imageUrl;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "product")
+    private List<ProductSpecification> productSpecifications;
     @ManyToOne(cascade = {CascadeType.PERSIST})
+    @JoinColumn(name = "category_id")
     private Category category;
 }
