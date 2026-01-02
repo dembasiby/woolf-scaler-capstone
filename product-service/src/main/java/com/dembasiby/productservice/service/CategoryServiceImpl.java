6a118c8dba68572f72ebfed5a6f475e3cbee602e
Demba Siby <dembasiby@gmail.com>
2026-01-02 16:43:34 +0000

    feat: add method to get products by category

--- product-service/src/main/java/com/dembasiby/productservice/service/CategoryServiceImpl.java
+++ product-service/src/main/java/com/dembasiby/productservice/service/CategoryServiceImpl.java
@@ -3,13 +3,11 @@ package com.dembasiby.productservice.service;
 import com.dembasiby.productservice.dto.category.CategoryDto;
 import com.dembasiby.productservice.dto.category.CreateCategoryDto;
 import com.dembasiby.productservice.dto.category.UpdateCategoryDto;
-import com.dembasiby.productservice.dto.product.ProductCategoryDto;
 import com.dembasiby.productservice.exception.ConflictException;
 import com.dembasiby.productservice.exception.NotFoundException;
 import com.dembasiby.productservice.mapper.CategoryMapper;
 import com.dembasiby.productservice.model.Category;
 import com.dembasiby.productservice.repository.CategoryRepository;
-import com.dembasiby.productservice.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +17,6 @@ import java.util.List;
 @AllArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
-    private final ProductRepository productRepository;
 
     @Override
     public CategoryDto createCategory(CreateCategoryDto createCategoryDto) {
@@ -60,20 +57,11 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public List<ProductCategoryDto> getCategoryProducts(Long id) {
-        Category category = categoryRepository.findByIdAndIsDeletedFalse(id)
-                .orElseThrow(() -> new NotFoundException("Category not found"));
-
-        return categoryRepository
-                .getFeaturedProductsByCategoryId(category.getId());
-    }
-
-    @Override
     public void deleteCategory(Long id) {
         Category category = categoryRepository.findByIdAndIsDeletedFalse(id)
                 .orElseThrow(() -> new NotFoundException("Category not found"));
 
-        if (productRepository.existsByCategoryIdAndIsDeletedFalse(id)) {
+        if (!category.getFeaturedProducts().isEmpty()) {
            throw new ConflictException("Category has active products");
         }
 
